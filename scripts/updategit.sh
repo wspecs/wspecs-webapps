b3d3e6eebb90703df7a8806bf0ae04838c9295ec
Wendly Saintil <wendly@servone.wspecs.com>
2017-09-30 01:14:09 +0300

    auto update 1506723249

--- scripts/updategit.sh
+++ scripts/updategit.sh
@@ -81,6 +81,7 @@ sed -i "s|basePath = '$1'|basePath = ''|g" $PUBLIC_DIR/**/*.ts
 
 sed -i "s|data/$1|data|g" $PUBLIC_DIR/data/*.js
 sed -i "s|\.\./dest/models/$1|dest/models|g" $PUBLIC_DIR/data/*.js
+sed -i "s|$1/||g" $PUBLIC_DIR/public/**/*.*
 sed -i "s|<%- base %>$1|<%- base %>|g" $PUBLIC_DIR/templates/*.ejs
 sed -i "s|<%- base %>$1|<%- base %>|g" $PUBLIC_DIR/templates/**/*.ejs
 sed -i "s|src=\"$1|src=\"|g" $PUBLIC_DIR/templates/*.ejs
